632525524c4f8efba8e8ccda8e15b349e65f8a3a
Dmitry <dsilichev@gmail.com>
2021-12-28 17:49:43 +0300

    lesson_3

--- SB/JS/03/index.js
+++ SB/JS/03/index.js
@@ -1,5 +1,43 @@
-let password = '1234_';
+// Задание 1
+let password = '|-__-|';
 
-if (password.length >= 4 && (password.includes('_') || password.includes('_'))){
-    console.log('ok');
+if (password.length >= 4 && (password.includes('_') || password.includes('-'))) {
+  console.log('Пароль надежный');
+}
+else {
+  console.log('Пароль недостаточно надежный');
+}
+
+// Задание 2
+let fname = 'NaMe';
+let sname = 'SuRNamE';
+
+function toUpper(str) {
+  let ustr = '';
+  for (i in str){
+    if (i == 0){
+      ustr += str[i].toUpperCase();
+    }
+    else{
+      ustr += str[i].toLowerCase();
+    }
+  }
+  console.log(ustr);
+}
+
+let ufname = toUpper(fname);
+let usname = toUpper(sname);
+
+if (fname === ufname){
+  console.log('Имя осталось без изменений');
+}
+else{
+  console.log('Имя было преобразовано');
+}
+
+if (sname === usname){
+  console.log('Фамилия осталось без изменений');
+}
+else{
+  console.log('Фамилия была преобразована');
 }
