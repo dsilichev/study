7f4b3eec9a450a92351994df7b6abb3fcf4b7eb9
Dmitry <dsilichev@gmail.com>
2022-06-07 16:40:54 +0300

    ITK_6

--- SB/ITK/6/server.js
+++ SB/ITK/6/server.js
@@ -1,16 +1,41 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((request, response) => {
+const delay = (ms) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms)
+  })
+}
+
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
+
+const server = http.createServer(async (request, response) => {
   switch (request.url) {
     case '/home': {
-      fs.readFile('pages/about.html', (err, data) => {
-        if (err) response.write(err)
-        else response.write(data)
-      })
-      //const data = fs.readFileSync('pages/about.html')
-      //response.write(data)
-      response.end()
+      try {
+        const data = await readFile('pages/about.html')
+        response.write(data);
+        response.end();
+      } catch (err) {
+        response.write(err.toString());
+        response.end();
+      }
+
+      break;
+    }
+    case '/about': {
+      await delay(3000);
+      response.write('about course');
+      response.end();
       break;
     }
     default: {
