dea7af41feafe772f1bc856a23d98d3e26204705
Dmitry <dsilichev@gmail.com>
2023-09-21 17:47:21 +0300

    close_by_clicking_out_of_modal

--- SB/JS_Pro/1/app.js
+++ SB/JS_Pro/1/app.js
@@ -1,11 +1,22 @@
 {
   const modal = document.getElementById("myModal");
+  const backdrop = document.getElementById("backdrop");
   const openButton = document.getElementById("openButton");
 
-  openButton.addEventListener('click', () => {
-    console.log('skdfj')
-    modal.style.display = 'block';
-    modal.classList.add('show');
-    modal.setAttribute('dialog');
-  })
+  openButton.addEventListener("click", () => {
+    modal.style.display = "block";
+    backdrop.style.display = 'block';
+    modal.classList.add("show");
+  });
+
+  function closeModal() {
+    modal.style.display = "none";
+    backdrop.style.display = 'none';
+    modal.classList.remove("show");
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      closeModal()
+    }
+  }
 }
