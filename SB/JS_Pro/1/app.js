cdd0134aa02f6c731266e68afad2952347bd8c6d
Dmitry <dsilichev@gmail.com>
2023-09-26 18:33:04 +0300

    input_form

--- SB/JS_Pro/1/app.js
+++ SB/JS_Pro/1/app.js
@@ -2,21 +2,35 @@
   const modal = document.getElementById("myModal");
   const backdrop = document.getElementById("backdrop");
   const openButton = document.getElementById("openButton");
-
+  const closeButton = document.getElementById("closeButton");
+  const closeX = document.getElementById("closeX");
+  
+  // show modal
   openButton.addEventListener("click", () => {
+    
     modal.style.display = "block";
     backdrop.style.display = 'block';
     modal.classList.add("show");
   });
 
+  // close modal
   function closeModal() {
     modal.style.display = "none";
     backdrop.style.display = 'none';
     modal.classList.remove("show");
   }
+  // close by clicking anywhere
   window.onclick = function(event) {
     if (event.target == modal) {
-      closeModal()
+      closeModal();
     }
   }
+  // close by clicking close button
+  closeButton.addEventListener('click', (e) => {
+    closeModal(e);
+  })  
+  // close by clicking X
+  closeX.addEventListener('click', (e) => {
+    closeModal(e);
+  }) 
 }
