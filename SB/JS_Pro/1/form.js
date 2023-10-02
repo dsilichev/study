20ef80761f8df95206fdeb991a63a28350def7ae
Dmitry <dsilichev@gmail.com>
2023-10-02 17:49:23 +0300

    to_fix_end_cut

--- SB/JS_Pro/1/form.js
+++ SB/JS_Pro/1/form.js
@@ -6,16 +6,24 @@
   const regex = /[\sа-яА-Я-]/g;
 
   firstName.addEventListener("input", () => {
-    //console.log(firstName.value.match(regex));
-    firstName.value = firstName.value.match(regex).join("");
+    firstName.value = (firstName.value.match(regex) || []).join("");
   });
 
   firstName.addEventListener("focusout", () => {
-    console.log(';sfrg')
+    console.log(";sfrg");
     // пробелы также как - удалить
-    firstName.value = firstName.value.trim().replaceAll(firstName.value.trim().match(/-+/), '-');
-    if (firstName.value.indexOf('-') === 0) {
-      firstName.value = firstName.value.slice(1);
+    firstName.value = firstName.value.replace(/-+/g, '-');
+    firstName.value = firstName.value.replace(/ +/g, ' ');
+    for (i=0; i<=1; i++) {
+      if (firstName.value.indexOf("-") === 0 || firstName.value.indexOf(" ") === 0) {
+        firstName.value = firstName.value.slice(1);
+        console.log(firstName.value)
+      }
+    }
+    for (i=0; i<=1; i++) {
+      if (firstName.value.indexOf("-") === firstName.value.length || firstName.value.indexOf(" ") === firstName.value.length) {
+        firstName.value = firstName.value.substring(0, firstName.value.length - 1); // need to fix
+      }
     }
   });
 }
